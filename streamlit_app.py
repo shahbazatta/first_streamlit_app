1127163136484a4496ae6bb86114b9a76b3f3ce8
shahbazatta <shahbaz.atta@gmail.com>
2022-10-27 16:23:08 +0500

    Update streamlit_app.py
    CSV loaded from the S3

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,11 @@
-
+import pandas
 import streamlit
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('Welcome to the Data Science World')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.dataframe(my_fruit_list)
