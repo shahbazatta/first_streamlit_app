f170e5e526ff362ebb5bc2aa083951da7634b607
shahbazatta <shahbaz.atta@gmail.com>
2022-11-04 16:27:58 +0500

    Update streamlit_app.py
    load fruit list

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,6 +6,9 @@ my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
 my_cur.execute("SELECT * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
+
+# Let's put a pick list here so they can pick the fruit they want to include 
+fruits_selected = streamlit.multiselect("What fruit whould you like information about?:", list(my_data_rows.index))
 streamlit.header("Fruit load list contains:")
 #streamlit.text(my_data_rows)
 streamlit.dataframe(my_data_rows)
