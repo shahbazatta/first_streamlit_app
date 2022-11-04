ce001d2d45f001d5c03811b9b8dbdce5ad9b5f03
shahbazatta <shahbaz.atta@gmail.com>
2022-11-04 16:35:41 +0500

    Update streamlit_app.py
    exception liberary added

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import pandas
 import streamlit
 import snowflake.connector
+from urllib.error import URLError
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
@@ -28,7 +29,7 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+fruit_choice = streamlit.text_input('What fruit would you like information about?',list(my_data_rows))
 streamlit.write('The user entered ', fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"kiwi")
