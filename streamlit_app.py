5715dcb689cc618a0dc6ef93e87c0acc54a40d43
shahbazatta <shahbaz.atta@gmail.com>
2022-11-04 15:57:56 +0500

    Update streamlit_app.py
    fetch data from the snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,12 @@ import snowflake.connector
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
-my_data_row = my_cur.fetchone()
-streamlit.text("Hello from Snowflake:")
-streamlit.text(my_data_row)
+my_cur.execute("SELECT * from fruit_load_list")
+my_data_rows = my_cur.fetchall()
+streamlit.header("Fruit load list contains:")
+streamlit.text(my_data_rows)
 
+streamlit.dataframe(my_data_row)
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.title('Welcome to the Data Science World')
 streamlit.header('Breakfast Menu')
